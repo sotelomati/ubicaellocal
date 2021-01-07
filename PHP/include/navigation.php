0a44c98f3ac77a88bed4edaa05f903b663a2d156
Day <daianrene99@gmail.com>
2021-01-07 20:25:56 -0300

    modified li

--- PHP/include/navigation.php
+++ PHP/include/navigation.php
@@ -4,7 +4,7 @@
 <li><i class="fas fa-map-marked-alt"></i></li>
 <li><a href="#">Ofertas</a></li>
 <li><a href="#">Gluten Free</a></li>
-<li><a href="#">El Noble</a></li>
+<li><a href="#">Servicio El Noble</a></li>
 <li><a href="#">Contacto</a></li>
 </ul>
 </div>
