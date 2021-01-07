77d8e0e68bfcf15ba90db9327d4f615fcdf2c8ca
Day <daianrene99@gmail.com>
2021-01-07 16:30:55 -0300

    Barra navegacion

---
+++ PHP/include/navigation.php
@@ -0,0 +1,8 @@
+<div id="navegador">
+<ul>
+<li><a href="#">Elemento 1</a></li>
+<li><a href="#">Elemento 2</a></li>
+<li><a href="#">Elemento 3</a></li>
+<li><a href="#">Elemento 4</a></li>
+</ul>
+</div>
