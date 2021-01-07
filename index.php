21c04d715c2112a2cf71d446439d2d45934c0744
Day <daianrene99@gmail.com>
2021-01-07 20:33:58 -0300

    prueba commits

--- index.php
+++ index.php
@@ -8,6 +8,6 @@
     include ("PHP/include/navigation.php");
     include ("PHP/include/footer.php");
     ?>
-    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nihil ad dignissimos consectetur nulla. Dolorem impedit, alias natus aspernatur veniam ipsam pariatur mollitia, quis eligendi, quisquam corporis eveniet unde ex.</p>
 </body>
 </html>
