dcbce31a76a5f8ee438d02abce4649a6852c681e
Matias Sotelo <sotelomati@gmail.com>
2021-01-07 16:12:18 -0300

    mas carpetas y hoja de estilos

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ubica el local</title>
+    <link rel="stylesheet" type="text/css" href="Estilos/normalize.css" media="screen" />
+</head>
+<body>
+    <?php
+    include ("PHP/include/header.php");
+
+
+    include ("PHP/include/footer.php");
+    ?>
+</body>
+</html>
