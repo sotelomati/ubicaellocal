072a083ec34743443f3a5a98cb8e68cefa6a2f8b
Matias Sotelo <sotelomati@gmail.com>
2021-01-07 21:53:26 -0300

    se siguio con footer

--- PHP/include/footer.php
+++ PHP/include/footer.php
@@ -3,21 +3,38 @@
         <div class="int-footer">
             <div class="row-footer">
                 <h3>Redes sociales</h3>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3392.2776627632884!2d-60.51704978430454!3d-31.76291202027793!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b44db836915947%3A0x10e7c34a3c41d7ac!2sBernardo%20O&#39;Higgins%20556%2C%20Paran%C3%A1%2C%20Entre%20R%C3%ADos!5e0!3m2!1ses-419!2sar!4v1610054256105!5m2!1ses-419!2sar" width="300" height="150" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    <ul class="des-ul">
+                        <li>Instagram: @Ubicaellocal</li>
+                        <li>Facebook: @Ubicaellocal</li>
+                        <li>Whastsapp: 03434708030</li>
+                    </ul>
             </div>
+
             <div class="row-footer">
-                <h3>Anexos</h3>
+                <h3>Drugstore: Zona de cobertura</h3>
+                <ul class="des-ul">
+                    <li>Fiambreria</li>
+                    <li>Dietetica</li>
+                    <li>Panaderia</li>
+                    <li>Confiteria</li>
+                    <li>Resto Bar</li>
+                </ul>
             </div>
+
             <div class="row-footer">
                 <h3>Desarrolladores</h3>
 
                 <ul class="des-ul">
-                    <li>Sotelo Matias Nicolas</li>
-                    <li>Rene Daian Lionel</li>
-                    <li>Bergomas Antu Francisco</li>
+                    <li>Sotelo Matias</li>
+                    <li>Rene Daian</li>
+                    <li>Bergomas Francisco</li>
                 </ul>
             </div>
         </div>
-        <p> Todos los derechos reservados | 2021 </p>
+        
+    </div>
+    <div class="pie-final">
+        
+    <p> Todos los derechos reservados | 2021 </p>
     </div>
 </div>
