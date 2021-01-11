b76913a55936c994346dbecda1f3a3d8e5d30b00
Day <daianrene99@gmail.com>
2021-01-11 14:27:50 -0300

    1101

--- pruebas.php
+++ pruebas.php
@@ -1,19 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<style>
+  .columnas{
+    color:white;
+    font-size: 40px;
+    border-right: 1px solid #d6d6d6;
+    text-align:center;
+    margin-bottom: 10px;
+  }
+
+  .columnas:hover {
+    color:#52c9cc
+  }
+
+  .columnas:last-child{
+    border:none;
+  }
+
+  .uel{
+    font-size: 125px;
+    text-align:center;
+    font-family: 'Brush Script MT';
+  }
+
+  .uel a{
+    color:white;
+  }
+
+ 
+</style>
+
 <body>
 
     <?php
     include ("PHP/include/head.php");
     ?>
 
-    <div class="container-fluid" style="font-size:40px; text-align:center;">
-        <div class="row">
-        <div class="col-12" style="background-color:orange; "> <a href="index.php" style=" text-decoration:none;"> Volver</a> </div>
-          <div class="col-12 col-md-6 col-lg-4" style="background-color:cyan; border: 5px solid blue">Ubica</div>
-          <div class="col-12 col-md-6 col-lg-4" style="background-color:lightgreen;">El</div>
-          <div class="col-12 col-md-12 col-lg-4" style="background-color:lightblue;">Local</div>
+    <div class="container-fluid" style="background-color: #212121" >
+
+        <div class="row justify-content-center">
+
+          <div class="col-12 uel" > <a href="index.php" style="text-decoration:none;"> Ubica el local</a> </div>
+    
+          <div class="col-12 col-md-6 col-lg-3 columnas" style="color:#f00">Ofertas</div>
+          <div class="col-12 col-md-6 col-lg-3 columnas" >Gluten Free</div>
+          <div class="col-12 col-md-6 col-lg-3 columnas" >Servicio El Noble</div>
+          <div class="col-12 col-md-6 col-lg-3 columnas" >Contacto</div>
+
         </div>
+
+          
     </div>
 
 
